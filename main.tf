207fb097d5d23b0abf9086b82b910552b85b1d3f
somnath <somnath23@gmail.com>
2026-04-25 23:02:26 +0530

    dddd

--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
-module "network" {
-  source = "./modules/network"
-
-  rg_name  = module.rg.rg_name
+module "rg" {
+  source   = "./modules/resource_group"
+  rg_name  = var.rg_name
   location = var.location
 }
 
+module "network" {
+  source      = "./modules/network"
+  rg_name     = module.rg.rg_name
+  location    = var.location
+  vnet_name   = "demo-vnet"
+  subnet_name = "demo-subnet"
+}
+
 module "vm" {
-  source = "./modules/windows_vm"
-
-  rg_name      = module.rg.rg_name
-  location     = var.location
-  subnet_id    = module.network.subnet_id
-  public_ip_id = module.network.public_ip_id
-
-  vm_name    = var.vm_name
-  admin_user = var.admin_user
-  admin_pass = var.admin_pass
+  source           = "./modules/windows_vm"
+  rg_name          = module.rg.rg_name
+  location         = var.location
+  subnet_id        = module.network.subnet_id
+  vm_name          = var.vm_name
+  admin_username   = var.admin_username
+  admin_password   = var.admin_password
 }
